6ba90fabd2e42532868f9569ee6d1fd904b5445c
Moritz <3672823+moritz31@users.noreply.github.com>
2021-12-13 17:01:33 +0100

    Finished day 6

--- 6_day/src/main/java/Day6.java
+++ 6_day/src/main/java/Day6.java
@@ -1,23 +1,20 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 public class Day6 {
     public static void main(String[] args) {
         String fishInput = "1,5,5,1,5,1,5,3,1,3,2,4,3,4,1,1,3,5,4,4,2,1,2,1,2,1,2,1,5,2,1,5,1,2,2,1,5,5,5,1,1,1,5,1,3,4,5,1,2,2,5,5,3,4,5,4,4,1,4,5,3,4,4,5,2,4,2,2,1,3,4,3,2,3,4,1,4,4,4,5,1,3,4,2,5,4,5,3,1,4,1,1,1,2,4,2,1,5,1,4,5,3,3,4,1,1,4,3,4,1,1,1,5,4,3,5,2,4,1,1,2,3,2,4,4,3,3,5,3,1,4,5,5,4,3,3,5,1,5,3,5,2,5,1,5,5,2,3,3,1,1,2,2,4,3,1,5,1,1,3,1,4,1,2,3,5,5,1,2,3,4,3,4,1,1,5,5,3,3,4,5,1,1,4,1,4,1,3,5,5,1,4,3,1,3,5,5,5,5,5,2,2,1,2,4,1,5,3,3,5,4,5,4,1,5,1,5,1,2,5,4,5,5,3,2,2,2,5,4,4,3,3,1,4,1,2,3,1,5,4,5,3,4,1,1,2,2,1,2,5,1,1,1,5,4,5,2,1,4,4,1,1,3,3,1,3,2,1,5,2,3,4,5,3,5,4,3,1,3,5,5,5,5,2,1,1,4,2,5,1,5,1,3,4,3,5,5,1,4,3";
 //        String fishInput = "3,4,3,1,2";
-        List<LanternFish> fishes = new ArrayList<>();
+        List<Long> fishes = Arrays.asList(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L);
 
-        String[] fishTimers = fishInput.split(",");
-        for (String time : fishTimers) {
-            fishes.add(new LanternFish(Integer.parseInt(time)));
+        Arrays.stream(fishInput.split(",")).forEach(input -> fishes.set(Integer.parseInt(input), fishes.get(Integer.parseInt(input)) + 1));
+
+        for(int i = 0; i <  256; i++) {
+            Collections.rotate(fishes,  -1);
+            fishes.set(6, fishes.get(6) + fishes.get(8));
         }
-
-        for (int i = 0; i < 200; i++) {
-            List<LanternFish> collect = fishes.stream().map(LanternFish::simulateDay).filter(Objects::nonNull).toList();
-            fishes.addAll(collect);
-            System.out.println(fishes.stream().count());
-        }
-
+        System.out.println(fishes.stream().reduce(0L, Long::sum));
     }
 }
